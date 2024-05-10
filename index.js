42f4e0ac86e56fb063665a83eb0a4cf7a516c1f9
fialh0 <giovanny.fialho@99jobs.com>
2024-05-10 14:52:18 -0300

    subindo projeto

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dataWL = require("./src/data/infos.json");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  return res.json(dataWL);
+});
+
+server.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  return res.json(dataWL.filter((info) => info.companyId == id)[0]);
+});
+
+server.listen(4000, () => {
+  console.log(`Server onnn::::`);
+});
